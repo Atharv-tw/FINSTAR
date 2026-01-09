90e92c911a9edf5c0d0859aaf05e218f52e7b2b2
Atharv Tiwari <tiwariatharv01042005@gmail.com>
2026-01-09 19:46:16 +0530

    Optimize Android build for low-memory systems
    - Reduce JVM heap from 8GB to 1.5GB
    - Limit metaspace to 384MB
    - Use serial GC for lower memory footprint
    - Disable Gradle daemon and parallel builds
    - Enable Gradle caching for faster rebuilds
    - Add dexOptions to limit DEX memory usage

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,6 +19,11 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    dexOptions {
+        javaMaxHeapSize = "1g"
+        preDexLibraries = false
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.finstar.finstar_app"
